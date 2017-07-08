9c5a58105a7ec183d8aefbd431e2a437a7a21965
Vivek Yadav <VivekYadavOfficial@users.noreply.github.com>
2017-07-08 21:17:00 +0530

    More code, create wordcloud
    Cleaning the text, creating corpus and creating wordcloud.

--- tweetanalysisinR.R
+++ tweetanalysisinR.R
@@ -1,5 +1,13 @@
+#vivekyadavofficial
+
 #install these required packages
 install.packages(c("devtools", "rjson", "bit64", "httr"))
+
+#install tm package for text mining
+install.packages("tm")
+
+#install wordcloud package for creating wordcloud
+install.packages("wordcloud")
 
 #load devtools library to install package from github
 library(devtools)
@@ -10,11 +18,49 @@ install_github("twitteR",username="geoffjentry")
 #create api key and others mentioned here by visiting dev.twitter.com
 api_key <- "XXXXXXXXXXXXX"
 api_secret <- "XXXXXXXXXXXXXXXX"
-acces_token <- "XXXXXXXXXXXXXXXXXXX"
+access_token <- "XXXXXXXXXXXXXXXXXXX"
 access_token_secret <- "XXXXXXXXXXXXXXXXXXXXX"
 
 #direct authentication
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
-#user authenticated, now you can search for tweets
-searchTwitter("#emmawatson")
+#user authenticated, now search and store the tweet data
+t <- searchTwitter("#emmawatson")
+
+#extract the text part of the tweet data
+t.text <- sapply(t, function(t) t$getText())
+
+#cleaning the text
+
+#convert the text to lowercase
+t.text <- tolower(t.text)
+                 
+#using gsub, replace the unwanted text parts
+#replace rt from text
+t.text <- gsub("rt", "", t.text)
+                 
+#replace usernames
+t.text <- gsub("@\\w+", "", t.text)
+                 
+#similarly replacing tabs, blanck spaces, link etc.
+t.text <- gsub("[[punct:]]", "", t.text)
+t.text <- gsub("http\\w+", "", t.text)
+t.text <- gsub("https\\w+", "", t.text)
+t.text <- gsub("[ |\t]{2,}", "", t.text)
+t.text <- gsub("^ ", "", t.text)
+t.text <- gsub(" $", "", t.text)
+                 
+#now load tm package
+library("tm")
+                 
+#create corpus of tweets
+t.text.corpus <- Corpus(VectorSource(t.text))
+                 
+#now remove stop words
+t.text.corpus <- tm_map(t.text.corpus, function(x) removeWords(x,stopwords()))
+                        
+#now load wordcloud package
+library(wordcloud)
+                        
+#now create the wordcloud of the frequent words in the tweets data
+wordcloud(t.text.corpus, min.freq=2, scale=c(8, 0.5), colors=brewer.pal(8, "Dark2"), random.color=TRUE, random.order=FALSE, max.words=40)
